8fcfae3f435f010d53f630b7c4a38d2aa0d507be
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2019-07-26 15:10:06 +0900

    蟻本の再帰の問題を二問実装した

---
+++ Others/Other/arihon/arihon_p34.py
@@ -0,0 +1,43 @@
+"""
+入力
+n: 整数の個数
+a: 整数列
+k: 和
+
+入力１
+4
+1 2 4 7
+13
+
+入力２
+4
+1 2 4 7
+15
+"""
+
+N = int(input())
+inputs = [int(i) for i in input().split()]
+K = int(input())
+
+def dfs(total, index):
+    if total == K:
+        return True
+    if index == N:
+        return False
+    
+    return dfs(total, index+1) or dfs(total+inputs[index], index+1)
+
+
+def dfs_sample(total, index):
+    if index == N:
+        return total == K
+    
+    if dfs(total, index+1):
+        return True
+    
+    if dfs(total+inputs[index], index+1):
+        return True
+    
+    return False
+
+print('Yes' if dfs(0, 0) else 'No')
---
+++ Others/Other/arihon/arihon_p35.py
@@ -0,0 +1,42 @@
+"""
+10 12
+W........WW.
+.WWW.....WWW
+....WW...WW.
+.........WW.
+.........W..
+..W......W..
+.W.W.....WW.
+W.W.W.....W.
+.W.W......W.
+..W.......W.
+"""
+
+import sys
+sys.setrecursionlimit(10**9)
+
+H, W = map(int, input().split())
+inputs = [list(input()) for _ in range(H)]
+
+vectors = []
+for x in range(-1, 2):
+    for y in range(-1, 2):
+        if x != 0 or y != 0:
+            vectors.append([x, y])
+
+def dfs(h, w):
+    inputs[h][w] = '.'
+    for vector in vectors:
+        next_h, next_w = h + vector[0], w + vector[1]
+        if 0 <= next_h < H and 0 <= next_w < W and inputs[next_h][next_w] == 'W':
+            dfs(next_h, next_w)
+    return
+
+ans = 0
+for h in range(H):
+    for w in range(W):
+        if inputs[h][w] == 'W':
+            dfs(h, w)
+            ans += 1
+
+print(ans)
