56056f0238d9b7ea77264f52e73df0df9fc7c239
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-04-10 04:46:13 +0900

    1問解いた

--- Others/Other/Codility/L5/lesson_5_4.py
+++ Others/Other/Codility/L5/lesson_5_4.py
@@ -1,6 +1,16 @@
+# DeepLと問題が違う場合があるので、制約などセンシティブな情報は必ず英語の問題文を読む
 def solution(A):
-    pass
-
+    ans = 0
+    counts = 0
+    for item in A[::-1]:
+        if item == 1:
+            counts += 1
+        else:
+            ans += counts
+    return -1 if ans > 1e9 else ans
 
 if __name__ == '__main__':
-    pass
+    # A = [0, 1, 0, 1, 1]
+    # A = [0, 0, 0, 0, 0]
+    A = [1, 1, 1, 1, 1]
+    print(solution(A))
